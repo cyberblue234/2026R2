4d41737879e0cc002b640a5740d25bbd20d58a29
Elliott Bunnell <elliottjbunnell@gmail.com>
2026-02-19 14:09:06 -0500

    Little bit of work

--- src/main/include/subsystems/Launcher.h
+++ src/main/include/subsystems/Launcher.h
@@ -5,6 +5,7 @@
 #include <ctre/phoenix6/TalonFX.hpp>
 #include <ctre/phoenix6/core/CoreTalonFX.hpp>
 #include <frc/PWM.h>
+#include <units/moment_of_inertia.h>
 #include "Constants.h"
 
 using namespace ctre::phoenix6;
@@ -30,4 +31,22 @@ private:
 
     frc::PWM actuator1{RobotMap::Launcher::kActuator1ID};
     frc::PWM actuator2{RobotMap::Launcher::kActuator2ID};
+};
+
+struct LauncherState
+{
+    units::degree_t pitch;
+    units::radians_per_second_t omega;
+};
+
+namespace LauncherConstants
+{
+    constexpr units::meter_t kFlywheelRadius = 2_in; // Need to check
+    constexpr units::meter_t kFuelRadius = 5.91_in / 2;
+    constexpr units::meter_t kCompression = 1_in;
+    constexpr units::meter_t kEffectiveFuelRadius = kFuelRadius - kCompression / 2;
+    constexpr units::kilogram_t kFlywheelMass = 0.98_lb;
+    // constexpr units::kilogram_square_meter_t = units::kilogram_square_meter_t{2};
+    constexpr units::kilogram_t kFuelMass = 0.5_lb;
+    constexpr double kLoss = 0; // 0% loss
 };
